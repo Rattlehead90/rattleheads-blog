0c4f88452cf0730a1f79d7dcb1e502491914abd8
Rattlehead90 <strogonov@seznam.cz>
2023-03-07 21:59:37 +0100

    Finish dropdown

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import { Transition } from "@headlessui/react";
 
 export default function Navbar() {
   
   return(
-    <div className="h-[80px] p-2  bg-gray-200">
+    <div className="h-fit p-2 pt-16">
       <div className="flex justify-between max-w-screen-md mx-auto">
         <div className="max-w-3xl flex flex-col">
           <h2 className="font-extrabold sm:text-4xl text-2xl">Rattlehead's Blog</h2>
@@ -15,28 +16,54 @@ export default function Navbar() {
             <Link href="/">About me</Link>
           </div>
         </div>
-        <DropDown>
-          <Menu />
-        </DropDown>
+        <div className="flex items-center gap-3">
+          <DropDown>
+            <Menu />
+          </DropDown>
+          <ThemeButton />
+        </div>
       </div>
     </div>
   );
+}
+
+function ThemeButton() {
+
+  return <Link href="#">
+    <Image
+      src="./icons/bright.svg"
+      width={32}
+      height={32}
+      alt="change theme" />
+  </Link>;
 }
 
 function DropDown(props) {
   const [open, setOpen] = useState(false);
 
   return (
-    <div>
+    <div className="flex items-center">
       <Link href="#" onClick={() => setOpen(!open)}>
         <Image
           src="./icons/caret.svg"
-          width={32}
-          height={32}
-          alt="dropdown arrow" />
+          className="hover:fill-blue-500"
+          width={42}
+          height={42}
+          alt="dropdown arrow"
+       />
       </Link>
 
-      { open && props.children }
+      <Transition
+        show={open}
+        enter="transition-opacity duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-300"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        { props.children }
+      </Transition>
     </div>
   );
 }
@@ -44,7 +71,7 @@ function DropDown(props) {
 function Menu() {
   function MenuItem(props) {
     return (
-      <Link href="#" className="h-12 flex items-center justify-start gap-3 hover:bg-gray-100 p-2 rounded-md transition-all">
+      <Link href={props.linkPage} className="h-12 flex items-center justify-start gap-3 hover:bg-gray-100 p-2 rounded-md transition-all">
         <span>{props.icon}</span>
         {props.children}
         <span className="ml-auto">{props.arrow}</span>
@@ -53,7 +80,7 @@ function Menu() {
   }
 
   return (
-    <div className="absolute border-gray-500 border-2 rounded-md p-4 top-[50px] shadow-lg w-64 -translate-x-56 overflow-hidden bg-white">
+    <div className="absolute border-gray-500 border-2 rounded-md p-4 top-[110px] shadow-lg w-56 -translate-x-56 overflow-hidden bg-white">
       <MenuItem 
         icon= {
           <Image
@@ -63,16 +90,9 @@ function Menu() {
             alt="github logo"
           />
         }
-        arrow= {
-          <Image
-          src="./icons/arrow.svg"
-          width={14}
-          className="rotate-180"
-          height={14}
-          alt="arrow pic" />
-        }
+        linkPage= "https://github.com/Rattlehead90"
       >
-        Where to find me
+        Github
       </MenuItem>
       <MenuItem 
         icon= {<Image
@@ -81,8 +101,9 @@ function Menu() {
                 height={32}
                 alt="github logo"
               />}
+        linkPage="https://www.linkedin.com/in/vadim-strogonov-3040b4169/"
       >
-        Change theme
+        LinkedIn
       </MenuItem>
     </div>
   );
